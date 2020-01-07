51ff083f5c93d916f7b5a3adc535aeed8a64f876
bijiaju <247636225@qq.com>
2020-01-07 11:00:11 +0800

    conf1

--- src/main/java/com/hp/docker_base/config/UpgradeVersion.java
+++ src/main/java/com/hp/docker_base/config/UpgradeVersion.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class UpgradeVersion implements ICommand {
     @Override
     public void operate() throws Exception {
-        String command="/conf/demo.sh";
+        String command="/conf/upgrade.sh";
         //需要传递的就是shell脚本的位置
         int retCode= JaveShellUtil.ExecCommand(command);
         System.out.println(retCode+"retCode");
